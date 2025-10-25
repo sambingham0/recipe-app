64b2f481f62fb7f2fbb8677e9d17315c941ad694
Sam Bingham <131708684+sambingham0@users.noreply.github.com>
2025-10-25 17:48:22 -0600

    render env

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (_req, res) => {
+  res.send('Recipe API is running successfully!');
+});
+
 mongoose.connect(process.env.MONGO_URI || '', {})
   .then(() => {
     console.log('MongoDB connected');
